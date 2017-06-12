9b404f8e51608269e934018b5ba274edafe5384f
ygorkagrishin <webygorkagrishin@gmail.com>
2017-06-13 00:08:58 +0300

    cloud

---
+++ common.js
@@ -0,0 +1,10 @@
+(function () {
+
+	setInterval( function () {
+
+		var cloud = document.querySelector( '.rain-cloud' ), a_class = "zevZ";
+		cloud.classList.toggle( a_class );
+
+	}, 3500);
+
+})();
